ecd2174bb704debf3c87e99c66a79a905bea8566
Schifferking <schifferking@hotmail.com>
2022-06-08 12:54:56 -0500

    Create get_coordinates method and game function
    The Board class now has a COORDINATES hash that it's a constant; it
    has some directions as keys that represent the positions on the board
    and a list with the row and column as values. This is used in the new
    get_coordinates method.
    
    The get_coordinates method receives a direction and then tries to get
    one value from the COORDINATES hash. In case the value is not found
    it returns the "unknown" string.
    
    The game function will play a tic-tac-toe game. For the moment it only
    creates two players, one with its mark; then it displays the board at
    the start of the game and after the first round. Also it keeps asking
    the player the position of the mark until it gives a valid direction.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -15,6 +15,15 @@ class Board
 
   def initialize
     @board = Array.new(3) {Array.new(3, nil)}
+    @COORDINATES = {"top left" => [0, 0],
+      "top center" => [0, 1],
+      "top right" => [0, 2],
+      "middle left" => [1, 0],
+      "middle center" => [1, 1],
+      "middle right" => [1, 2],
+      "bottom left" => [2, 0],
+      "bottom center" => [2, 1],
+      "bottom right" => [2, 2]}
   end
 
   def is_cell_empty?(row, column)
@@ -26,6 +35,7 @@ class Board
   end
 
   def display_board
+    puts ""
     board.each_with_index do |row, row_index|
       print " "
       row.each_with_index do |column, column_index|
@@ -41,4 +51,31 @@ class Board
     end
     puts ""
   end
+
+  def get_coordinates(direction)
+    @COORDINATES.fetch(direction, "unknown")
+  end
 end
+
+def game
+  p1 = Player.new('X')
+  p2 = Player.new('O')
+  b = Board.new
+
+  b.display_board
+  puts "Where do you want to place your mark?"
+  p_direction = gets.chomp.downcase
+  p_coordinates = b.get_coordinates(p_direction)
+  puts ""
+  while p_coordinates == "unknown"
+    puts "Please enter a valid direction"
+    p_direction = gets.chomp.downcase
+    p_coordinates = b.get_coordinates(p_direction)
+    puts ""
+  end
+
+  p1.fill_cell(b, p_coordinates.first, p_coordinates.last)
+  b.display_board
+end
+
+game
