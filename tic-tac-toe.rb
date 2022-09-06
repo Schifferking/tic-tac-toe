729f5d354b313ad5c1bf25b4658061da4ec470d5
Schifferking <schifferking@hotmail.com>
2022-09-06 17:05:52 -0500

    Refactor code

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -5,7 +5,7 @@ class Player
     @mark = mark
   end
 
-  def fill_cell(board, row, column)    
+  def fill_cell(board, row, column)
     board.board[row][column] = mark
   end
 
@@ -18,72 +18,72 @@ class Board
   attr_accessor :board, :lines
 
   def initialize
-    @board = Array.new(3) {Array.new(3, nil)}
-    @COORDINATES = {"top left" => [0, 0],
-      "top center" => [0, 1],
-      "top right" => [0, 2],
-      "middle left" => [1, 0],
-      "middle center" => [1, 1],
-      "middle right" => [1, 2],
-      "bottom left" => [2, 0],
-      "bottom center" => [2, 1],
-      "bottom right" => [2, 2]}
+    @board = Array.new(3) { Array.new(3, nil) }
+    @COORDINATES = { 'top left' => [0, 0],
+                     'top center' => [0, 1],
+                     'top right' => [0, 2],
+                     'middle left' => [1, 0],
+                     'middle center' => [1, 1],
+                     'middle right' => [1, 2],
+                     'bottom left' => [2, 0],
+                     'bottom center' => [2, 1],
+                     'bottom right' => [2, 2] }
   end
 
   def is_cell_filled?(row, column)
     return board[row][column] != nil
   end
-  
+
   def is_board_full?
     board.flatten.none?(nil)
   end
 
   def display_board
-    puts ""
+    puts ''
     board.each_with_index do |row, row_index|
-      print " "
+      print ' '
       row.each_with_index do |column, column_index|
         if !is_cell_filled?(row_index, column_index)
-          print "  "
+          print '  '
         else
-          print board[row_index][column_index] + " "
+          print board[row_index][column_index] + ' '
         end
-        print "| " if column_index <= 1
+        print '| ' if column_index <= 1
       end
       print "\n"
       print " ---------\n" if row_index <= 1
     end
-    puts ""
+    puts ''
   end
 
   def get_coordinates(direction)
-    @COORDINATES.fetch(direction, "unknown")
+    @COORDINATES.fetch(direction, 'unknown')
   end
 
   def update_lines
-    @lines = {"top row" => board[0],
-      "middle row" => board[1],
-      "bottom row" => board[2],
-      "left column" => [board[0][0], board[1][0], board[2][0]],
-      "middle column" => [board[0][1], board[1][1], board[2][1]],
-      "right column" => [board[0][2], board[1][1], board[2][2]],
-      "left diagonal" => [board[0][0], board[1][1], board[2][2]],
-      "right diagonal" => [board[0][2], board[1][1], board[2][0]],}
+    @lines = { 'top row' => board[0],
+               'middle row' => board[1],
+               'bottom row' => board[2],
+               'left column' => [board[0][0], board[1][0], board[2][0]],
+               'middle column' => [board[0][1], board[1][1], board[2][1]],
+               'right column' => [board[0][2], board[1][1], board[2][2]],
+               'left diagonal' => [board[0][0], board[1][1], board[2][2]],
+               'right diagonal' => [board[0][2], board[1][1], board[2][0]], }
   end
 
   def is_line_filled?
     update_lines
 
     lines.each do |key, value|
-       if lines[key].all?('X')
-         return true
-       elsif lines[key].all?('O')
-         return true
-       end
+      if lines[key].all?('X')
+        return true
+      elsif lines[key].all?('O')
+        return true
+      end
     end
-    
-    return false
-  end  
+
+    false
+  end
 end
 
 def game
@@ -93,40 +93,40 @@ def game
 
   [p1, p2].cycle.first(9).each do |player|
     # Start of round
-    puts "#{player}'s turn"    
-  
+    puts "#{player}'s turn"
+
     b.display_board
-    puts "Where do you want to place your mark?"
+    puts 'Where do you want to place your mark?'
     p_direction = gets.chomp.downcase
     p_coordinates = b.get_coordinates(p_direction)
-    puts ""
-    
+    puts ''
+
     # Validate player's input
-    while p_coordinates == "unknown" || b.is_cell_filled?(p_coordinates.first, p_coordinates.last)
-      puts "Please enter a valid direction"
+    while p_coordinates == 'unknown' || b.is_cell_filled?(p_coordinates.first, p_coordinates.last)
+      puts 'Please enter a valid direction'
       p_direction = gets.chomp.downcase
       p_coordinates = b.get_coordinates(p_direction)
-      puts ""
-      if p_coordinates != "unknown"
-        if !b.is_cell_filled?(p_coordinates.first, p_coordinates.last)
+      puts ''
+      if p_coordinates != 'unknown'
+        unless b.is_cell_filled?(p_coordinates.first, p_coordinates.last)
           break
         end
       end
     end
-    
+
     player.fill_cell(b, p_coordinates.first, p_coordinates.last)
 
     # Winner
     if b.is_line_filled?
       b.display_board
-      
+
       puts "#{player} won!"
       break
     end
   end
 
   # Tie
-  if !b.is_line_filled?
+  unless b.is_line_filled?
     b.display_board
 
     puts "It's a tie!"
