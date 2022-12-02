84af65f7884e2e3b3712af95478aa4670d1c5855
Schifferking <schifferking@hotmail.com>
2022-12-02 13:26:08 -0600

    Delete Player class code

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -1,25 +1,5 @@
 require_relative '../lib/player'
 
-class Player
-  attr_reader :mark
-
-  def initialize(mark)
-    @mark = mark
-  end
-
-  def fill_cell(board, row, column)
-    board.fill_cell(row, column, mark)
-  end
-
-  def to_s
-    @mark
-  end
-
-  def enter_coordinates
-    gets.chomp.downcase
-  end
-end
-
 class Board
   attr_accessor :board, :lines
 
