b190999ec716f0a86bd1c992f28048ee4252f269
Schifferking <schifferking@hotmail.com>
2022-12-05 15:25:50 -0600

    Add board_full? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -43,4 +43,55 @@ describe Board do
       end
     end
   end
+
+  describe '#board_full?' do
+    context 'when the board at least has an empty cell' do
+      subject(:board_not_full) { described_class.new }
+
+      before do
+        x_mark = 'X'
+        o_mark = 'O'
+        # Fill cells
+        board_not_full.board[0][0] = x_mark
+        board_not_full.board[0][1] = o_mark
+        board_not_full.board[0][2] = o_mark
+
+        board_not_full.board[1][0] = x_mark
+        board_not_full.board[1][1] = x_mark
+        board_not_full.board[1][2] = o_mark
+
+        board_not_full.board[2][0] = o_mark
+        board_not_full.board[2][1] = x_mark
+      end
+
+      it 'returns false' do
+        expect(board_not_full).not_to be_board_full
+      end
+    end
+
+    context 'when the board is full' do
+      subject(:board_already_full) { described_class.new }
+
+      before do
+        x_mark = 'X'
+        o_mark = 'O'
+        # Fill board
+        board_already_full.board[0][0] = x_mark
+        board_already_full.board[0][1] = o_mark
+        board_already_full.board[0][2] = o_mark
+
+        board_already_full.board[1][0] = o_mark
+        board_already_full.board[1][1] = o_mark
+        board_already_full.board[1][2] = x_mark
+
+        board_already_full.board[2][0] = x_mark
+        board_already_full.board[2][1] = x_mark
+        board_already_full.board[2][2] = o_mark
+      end
+
+      it 'returns true' do
+        expect(board_already_full).to be_board_full
+      end
+    end
+  end
 end
