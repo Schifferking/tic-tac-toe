84692f444fd94269e09f226742e63ba4822bbed8
Schifferking <schifferking@hotmail.com>
2022-12-05 15:09:15 -0600

    Add cell_filled? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,4 +14,33 @@ describe Board do
       end
     end
   end
+
+  describe '#cell_filled?' do
+    context 'when a cell has no value' do
+      subject(:board_cell_empty) { described_class.new }
+
+      it 'returns false' do
+        row = 2
+        column = 1
+        expect(board_cell_empty.cell_filled?(row, column)).to be false
+      end
+    end
+
+    context 'when a cell has a value' do
+      subject(:board_cell_value) { described_class.new }
+
+      before do
+        row = 2
+        column = 0
+        mark = 'X'
+        board_cell_value.board[row][column] = mark
+      end
+
+      it 'returns true' do
+        row = 2
+        column = 0
+        expect(board_cell_value.cell_filled?(row, column)).to be true
+      end
+    end
+  end
 end
