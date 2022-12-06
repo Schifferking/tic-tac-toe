0ae9c9bf61a4bfcca2fc3788dd1d6c454b68a405
Schifferking <schifferking@hotmail.com>
2022-12-06 17:39:20 -0600

    Add validate_number tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -24,4 +24,33 @@ describe TicTacToe do
       end
     end
   end
+
+  describe '#validate_number' do
+    context 'when the number is lesser than 1' do
+      subject(:tic_tac_toe_below_one) { described_class.new }
+
+      it 'returns nil' do
+        number = 0
+        expect(tic_tac_toe_below_one.validate_number(number)).to be nil
+      end
+    end
+
+    context 'when the number is greater than 9' do
+      subject(:tic_tac_toe_above_nine) { described_class.new }
+
+      it 'returns nil' do
+        number = 10
+        expect(tic_tac_toe_above_nine.validate_number(number)).to be nil
+      end
+    end
+
+    context 'when the number is between 1 and 9' do
+      subject(:tic_tac_toe_valid_number) { described_class.new }
+
+      it 'returns the number' do
+        number = 7
+        expect(tic_tac_toe_valid_number.validate_number(number)).to eq number
+      end
+    end
+  end
 end
