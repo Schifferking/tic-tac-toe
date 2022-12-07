869f6bf8ab65ce43f2ad25a427648fdb9a52dd8d
Schifferking <schifferking@hotmail.com>
2022-12-07 14:45:52 -0600

    Add coordinates_hash method
    This method returns a hash that contains a pair of a number and a list
    with the coordinates of a board's cell. It's used to know the cell a
    player wants to fill.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,18 @@ class TicTacToe
   def fill_cell(row, column, mark)
     @board.fill_cell(row, column, mark)
   end
+
+  def coordinates_hash
+    { 1 => [0, 0],
+      2 => [0, 1],
+      3 => [0, 2],
+      4 => [1, 0],
+      5 => [1, 1],
+      6 => [1, 2],
+      7 => [2, 0],
+      8 => [2, 1],
+      9 => [2, 2] }
+  end
 end
 
 def game
