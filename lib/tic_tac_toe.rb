493997cdb5fad65d295dd966f72b2cc5ccbaa638
Schifferking <schifferking@hotmail.com>
2022-12-08 14:44:22 -0600

    Delete print_welcome_message method
    Moved to Printable module.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,10 +12,6 @@ class TicTacToe
     @board = Board.new
   end
 
-  def print_welcome_message
-    puts 'Welcome to a new Tic tac toe game!'
-  end
-
   def print_current_player_turn(player)
     puts "#{player}'s turn"
   end
