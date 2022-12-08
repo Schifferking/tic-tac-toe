a884ba39fbb81e7b638d0183d3f58b6523f61fc7
Schifferking <schifferking@hotmail.com>
2022-12-08 14:47:43 -0600

    Delete print_current_player_turn method
    Moved to printable.rb

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,10 +12,6 @@ class TicTacToe
     @board = Board.new
   end
 
-  def print_current_player_turn(player)
-    puts "#{player}'s turn"
-  end
-
   def print_prompt_message
     puts 'Please select a number between 1 and 9 to place a mark'
     puts '1 means the top left cell and 9 means the bottom right cell'
