2d18313c44d763b54888d47a55ec9684c55b55f2
Schifferking <schifferking@hotmail.com>
2022-12-08 14:46:43 -0600

    Copy print_current_player_turn method
    Copied from tic_tac_toe.rb

--- lib/printable.rb
+++ lib/printable.rb
@@ -2,4 +2,8 @@ module Printable
   def print_welcome_message
     puts 'Welcome to a new Tic tac toe game!'
   end
+
+  def print_current_player_turn(player)
+    puts "#{player}'s turn"
+  end
 end
