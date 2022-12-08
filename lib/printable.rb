e3c791d2e8dee69a877e71fb1466ac17265d5189
Schifferking <schifferking@hotmail.com>
2022-12-08 14:48:28 -0600

    Copy print_prompt_message method
    Copied from tic_tac_toe.rb

--- lib/printable.rb
+++ lib/printable.rb
@@ -6,4 +6,9 @@ module Printable
   def print_current_player_turn(player)
     puts "#{player}'s turn"
   end
+
+  def print_prompt_message
+    puts 'Please select a number between 1 and 9 to place a mark'
+    puts '1 means the top left cell and 9 means the bottom right cell'
+  end
 end
