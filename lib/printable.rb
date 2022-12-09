a1eefba33d53fba7b34ec6251501c819a3d9b9cd
Schifferking <schifferking@hotmail.com>
2022-12-09 15:12:34 -0600

    Copy print_square_content method

--- lib/printable.rb
+++ lib/printable.rb
@@ -32,4 +32,13 @@ module Printable
   def print_empty_space
     print ' '
   end
+
+  def print_square_content(row, column)
+    if !cell_filled?(row, column)
+      print_empty_space
+    else
+      print board[row][column].to_s
+    end
+    print_empty_space
+  end
 end
